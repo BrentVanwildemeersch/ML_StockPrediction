dcb1651999de7a0e5a9c7d6d6e04ef7bf5aafa10
BrentVanwildemeersch <brent.vanwildemeersch@student.howest.be>
2017-01-06 05:52:55 -0800

    Get data

--- Flask Test/flasktest.py
+++ Flask Test/flasktest.py
@@ -1,5 +1,7 @@
 from flask import Flask, request
 from flask import render_template
+import time
+from datetime import datetime,timedelta
 
 app = Flask(__name__)
 
@@ -11,12 +13,21 @@ def hello():
 def recieveData():
     symbol = request.json['code']
     time = request.json['time']
-    return  symbol
+
+
+    return str(time)
+
+def getFinancialData(symbol, Day_amount):
+        stocks = [symbol]
+        endDate = datetime.now().date()
+        startDate = (endDate - timedelta(days=int(Day_amount)))
+
+
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
 
 
 
-def getFinancialData(symbol, date):
-    print "Test"
