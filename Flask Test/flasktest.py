eb32e5a76e60845799dd804a0f7d41a7822b5b1e
BrentVanwildemeersch <brent.vanwildemeersch@student.howest.be>
2017-01-06 02:21:38 -0800

    Add files via upload

---
+++ Flask Test/flasktest.py
@@ -0,0 +1,18 @@
+from flask import Flask, request
+from flask import render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return render_template("index.html")
+
+@app.route("/receiveData", methods=['POST'])
+def recieveData():
+    symbol = request.json['code']
+    symbol = request.json['time']
+    return
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
