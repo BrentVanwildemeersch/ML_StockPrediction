59803e018998466738ecb4ba85ba1d687db40f05
BrentVanwildemeersch <brent.vanwildemeersch@student.howest.be>
2017-01-06 08:25:10 -0800

    Get data

--- Flask Test/flasktest.py
+++ Flask Test/flasktest.py
@@ -2,6 +2,7 @@ from flask import Flask, request
 from flask import render_template
 import time
 from datetime import datetime,timedelta
+import pandas_datareader.data as web
 
 app = Flask(__name__)
 
@@ -13,14 +14,24 @@ def hello():
 def recieveData():
     symbol = request.json['code']
     time = request.json['time']
+    data = getFinancialData(symbol,time)
 
-
-    return str(time)
+    return data
 
 def getFinancialData(symbol, Day_amount):
         stocks = [symbol]
         endDate = datetime.now().date()
         startDate = (endDate - timedelta(days=int(Day_amount)))
+
+        df = web.DataReader(symbol,"yahoo",startDate,endDate)
+        dates =[]
+        for x in range(len(df)):
+            newdate = str(df.index[x])
+            newdate = newdate[0:10]
+            dates.append(newdate)
+        df['dates'] = dates
+
+        return df
 
 
 if __name__ == "__main__":
