19d1ac820014d69378d1cb6e314be51ec10c4fb8
BrentVanwildemeersch <brent.vanwildemeersch@student.howest.be>
2017-01-07 06:47:23 -0800

    Predict open value next day + predict low high value next day

--- Flask Test/flasktest.py
+++ Flask Test/flasktest.py
@@ -31,34 +31,70 @@ def getFinancialData(symbol, Day_amount):
         df = web.DataReader(symbol,"yahoo",startDate,endDate)
         df =df[['Open','Low','High','Close']]
 
-        X = df.ix[:,0:1]
-        Y = df.ix[:,3:4]
+        # first test lineaire regressie voor stock prediction
+        #
+        # X = df.ix[:,0:1]
+        # Y = df.ix[:,3:4]
+        #
+        # from sklearn.cross_validation import train_test_split
+        #
+        # X_train, X_test,Y_train,Y_test = train_test_split(X,Y,test_size=0.3)
+        #
+        #
+        #
+        # regr_bp = linear_model.LinearRegression()
+        # regr_bp.fit(X_train,Y_train)
+        #
+        # # open today == close yesteraday
+        # openTdy = Y.iloc[len(X)-1][0]
+        # # predict today
+        # closeTdy =regr_bp.predict(openTdy)
+        #
+        # # open tommorow = close today
+        # # predict close tomorrow
+        # closeTomorrow= regr_bp.predict(closeTdy)
+        #
+        # # predict +1day
+        # close_2day = regr_bp.predict(closeTomorrow)
+        #
+        #
+        #
+        #
+        # return close_2day
+
+#        second test
+
+
+# voorspellen van de openingswaarde van het aandeel
+        df_predictOpen = df[['Low','High','Close','Open']]
+        df_predictOpen.Open = df_predictOpen.Open.shift(-1)
+
+        df_predictOpen=df_predictOpen.ix[:-1]
+
+        # multiple regression, importeren van gekwadrateerde variabelen
+        df_predictOpen.insert(1,'Low x 2',df_predictOpen.Low**2)
+        df_predictOpen.insert(3, 'High x 2', df_predictOpen.High ** 2)
+        # df_predictOpen.insert(5, 'Close x 2', df_predictOpen.Close ** 2)
+
+        # opdelen van dataframe in X & Y
+        X_predictOpen = df_predictOpen.ix[:,0:5]
+        Y_predictOpen = df_predictOpen.ix[:,5:6]
 
         from sklearn.cross_validation import train_test_split
+        X_train,X_test,Y_train,Y_test = train_test_split(X_predictOpen,Y_predictOpen, test_size=0.5)
 
-        X_train, X_test,Y_train,Y_test = train_test_split(X,Y,test_size=0.8)
+        regr_open = linear_model.LinearRegression()
+        regr_open.fit(X_train,Y_train)
+        score = regr_open.score(X_test,Y_test)
 
+        # voorspellen openingswaarde nieuwe dag
+        # return X_predictOpen
+        # return regr_open.predict([[41.119999,1690.854318,41.830002,1749.749067,41.599998]])
 
-
-        regr_bp = linear_model.LinearRegression()
-        regr_bp.fit(X_train,Y_train)
-
-        # open today == close yesteraday
-        openTdy = Y.iloc[len(X)-1][0]
-        # predict today
-        closeTdy =regr_bp.predict(openTdy)
-
-        # open tommorow = close today
-        # predict close tomorrow
-        closeTomorrow= regr_bp.predict(closeTdy)
-
-        # predict +1day
-        close_2day = regr_bp.predict(closeTomorrow)
-
-
-
-
-        return close_2day
+# voorspellen van de low en high variabelen van de nieuwe dag
+        df_predictLowHigh = df[['Close','Open','High','Low']]
+        df_predictLowHigh = df_predictLowHigh.Close.shift(1)
+        return df_predictLowHigh
 
 
 
